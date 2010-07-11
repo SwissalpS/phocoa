0baf3a4db763dac79751cead0b9f6fa97931638c
SwissalpS <Luke@SwissalpS.ws>
2010-07-11 17:28:10 +0200

    not needed proposition put on hold

--- phocoa/framework/script/scriptComponents.php
+++ phocoa/framework/script/scriptComponents.php
@@ -52,6 +52,55 @@ class module_{$modName}_{$pageName}
     $moduleTemplate .= "
 ?>";
 
+	// 20100510_023949 SwissalpS added checking in which dir we are but decided to not use scripts/phocoa phing thingy for this simple stuff
+	/*
+	$sCurrDir = getcwd();
+	if ('modules' == basename($sCurrDir)) {
+		// good
+	} else {
+		print 'not in modules directory, searching upward' . chr(10);
+		$sPossibleModuleDir = $sCurr . DIRECTORY_SEPARATOR . 'modules';
+		if (isdir($sPossibleModuleDir)) {
+			print 'changing to a possible dir: ' . $sPossibleModuleDir . chr(10);
+			chdir($sPossibleModuleDir);
+
+		} else {
+			print 'not upward, well maybe closer to root' . chr(10) . 'checking if APP_ROOT is defined' . chr(10);
+			if (defined('APP_ROOT')) {
+				$sPossibleModuleDir = APP_ROOT . DIRECTORY_SEPARATOR . 'modules';
+				if (isdir($sPossibleModuleDir)) {
+					if (false === strstr($sCurrDir, APP_ROOT)) {
+						print 'APP_ROOT is defined but we are *not* in its subtree. Bailing.' . chr(10); // TODO: ask if ok or ask for path...but we don't know... cli/cgi/eval.. call
+						return;
+
+					} else {
+						print 'APP_ROOT is defined and we are in its subtree -> changing to dir: ' . $sPossibleModuleDir . chr(10);
+						chdir($sPossibleModuleDir);
+
+					} // if in subtree of APP_ROOT
+
+				} else {
+					print 'APP_ROOT is defined but no modules dir. Bailing.' . chr(10);
+					return;
+
+				} // if modules dir exists in APP_ROOT
+
+			} else {
+				// APP_ROOT is not defined -> search toward root
+				print 'APP_ROOT is *not* defined';
+				$mPos = strstr($sCurrDir, DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR);
+				if (false === $mPos) {
+					print ' and we are not in a subdir of /modules/. Bailing' . chr(10);
+					return;
+
+				} else {
+				} // if 'modules' in path somewhere
+
+			} // if got APP_ROOT -> ##PHOCOA_APP_DIR##
+
+		} // if upwards
+	} // if ok or need to change
+	*/
     // check and make dir
     if (!file_exists("./$modName"))
     {
