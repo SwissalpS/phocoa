ad15b764ea6e5affec271040a6b408fc3c3deaf8
SwissalpS <Luke@SwissalpS.ws>
2011-01-12 03:27:48 +0100

    refactored scriptComponents for more flexible
    reuse of module and page delegate classes
    helps a lot in creating pages and adding to module

--- phocoa/framework/script/scriptComponents.php
+++ phocoa/framework/script/scriptComponents.php
@@ -1,199 +1,137 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
 
-function createModule($modName, $pageName)
-{
+function createModule($modName, $pageName) {
 
-    $moduleTemplate = "<?php
+	$moduleTemplate = '<' . '?' . 'php
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
+' . _moduleBit($modName, $pageName);
 
-class module_{$modName} extends WFModule
-{
-    function sharedInstancesDidLoad()
-    {
-    }
-";
+	if ($pageName !== NULL)
+		$moduleTemplate .= '
+' . _pageForModule($modName, $pageName);
 
-    if ($pageName === NULL)
-    {
-        $moduleTemplate .= "
-    function defaultPage()
-    {
-        return 'defaultPage';
-    }
-";
-    }
-    else
-    {
-        $moduleTemplate .= "
-    function defaultPage()
-    {
-        return '{$pageName}';
-    }
-";
-    }
-    $moduleTemplate .= "
-}
-";
-    
-    if ($pageName !== NULL)
-    {
-        $moduleTemplate .= "
+	$moduleTemplate .= '
+?' . '>';
 
-class module_{$modName}_{$pageName}
-{
-    public function parameterList()
-    {
-        return array();
-    }
-}
-";
-    }
-    $moduleTemplate .= "
-?>";
+	// check and make dir
+	if (!file_exists("./$modName")) mkdir('./' . $modName);
 
-	// 20100510_023949 SwissalpS added checking in which dir we are but decided to not use scripts/phocoa phing thingy for this simple stuff
-	/*
-	$sCurrDir = getcwd();
-	if ('modules' == basename($sCurrDir)) {
-		// good
+	$modFilePath = "{$modName}/{$modName}.php";
+	if (!file_exists($modFilePath))	{
+		print "Writing $modFilePath\n";
+		file_put_contents($modFilePath, $moduleTemplate);
 	} else {
-		print 'not in modules directory, searching upward' . chr(10);
-		$sPossibleModuleDir = $sCurr . DIRECTORY_SEPARATOR . 'modules';
-		if (isdir($sPossibleModuleDir)) {
-			print 'changing to a possible dir: ' . $sPossibleModuleDir . chr(10);
-			chdir($sPossibleModuleDir);
+		print "Skipping module $modName because it already exists.\n";
+	}
 
-		} else {
-			print 'not upward, well maybe closer to root' . chr(10) . 'checking if APP_ROOT is defined' . chr(10);
-			if (defined('APP_ROOT')) {
-				$sPossibleModuleDir = APP_ROOT . DIRECTORY_SEPARATOR . 'modules';
-				if (isdir($sPossibleModuleDir)) {
-					if (false === strstr($sCurrDir, APP_ROOT)) {
-						print 'APP_ROOT is defined but we are *not* in its subtree. Bailing.' . chr(10); // TODO: ask if ok or ask for path...but we don't know... cli/cgi/eval.. call
-						return;
+	print "Done building module $modName!\n";
 
-					} else {
-						print 'APP_ROOT is defined and we are in its subtree -> changing to dir: ' . $sPossibleModuleDir . chr(10);
-						chdir($sPossibleModuleDir);
+} // createModule
 
-					} // if in subtree of APP_ROOT
 
-				} else {
-					print 'APP_ROOT is defined but no modules dir. Bailing.' . chr(10);
-					return;
+function _moduleBit($modName, $pageName) {
 
-				} // if modules dir exists in APP_ROOT
+	 $moduleTemplate = '
+class module_' . $modName . ' extends WFModule {
 
-			} else {
-				// APP_ROOT is not defined -> search toward root
-				print 'APP_ROOT is *not* defined';
-				$mPos = strstr($sCurrDir, DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR);
-				if (false === $mPos) {
-					print ' and we are not in a subdir of /modules/. Bailing' . chr(10);
-					return;
+	function sharedInstancesDidLoad() {
 
-				} else {
-				} // if 'modules' in path somewhere
 
-			} // if got APP_ROOT -> ##PHOCOA_APP_DIR##
 
-		} // if upwards
-	} // if ok or need to change
-	*/
-    // check and make dir
-    if (!file_exists("./$modName"))
-    {
-        mkdir('./' . $modName);
-    }
-    $modFilePath = "{$modName}/{$modName}.php";
-    if (!file_exists($modFilePath))
-    {
-        print "Writing $modFilePath\n";
-        file_put_contents($modFilePath, $moduleTemplate);
-    }
-    else
-    {
-        print "Skipping module $modName because it already exists.\n";
-    }
+	} // sharedInstancesDidLoad
 
-    print "Done building module $modName!\n";
-}
 
-function createPage($pageName)
-{
-    $includeHints = false;
-    if ($includeHints)
-    {
-        $configFile = "<?php
-        /* vim: set expandtab tabstop=4 shiftwidth=4 syntax=php: */
-        \$__config = array(
-                    'instance_or_controller_id' => array(
-                            'properties' => array(
-                                'propName' => 'propValue'
-                                ),
-                            'bindings' => array(
-                                'propName' => array(
-                                    'instanceID' => 'controller instance id',       // or can be '#module#' to bind to the current module
-                                    'controllerKey' => 'the controller key to use',
-                                    'modelKeyPath' => 'the modelKeyPath to use',
-                                    'options' => array(
-                                        'bindingOption1' => 'bindingOptionValue'
-                                        )
-                                    )
-                                )
-                        )
-                );
+	// this function should throw an exception if the user is not permitted to edit (add/edit/delete) in the current context
+	function verifyEditingPermission($oPage) {
 
-        ?>";
+		// example
+		// $oAauthInfo = WFAuthorizationManager::sharedAuthorizationManager()->authorizationInfo();
 
-        $instancesFile = "<?php
-        /* vim: set expandtab tabstop=4 shiftwidth=4 syntax=php: */
+		// if ($oAauthInfo->userid() != $oPage->sharedOutlet(\'sharedEntityId\')->selection()->getUserId()) throw(new Exception(\'You don\'t have permission to edit\'));
 
-        \$__instances = array(
-                'widgetID' => array(
-                    'class' => 'WFWidget subclass',
-                    'children' => array()
-                    ),
-                'controllerID' => array('class' => 'WFObjectController subclass')
-                );
+	} // verifyEditingPermission
+';
 
-        ?>";
+	if (NULL === $pageName) $pageName = 'defaultPage';
 
-        $templateFile = "
-{* vim: set expandtab tabstop=4 shiftwidth=4 syntax=smarty: *}
-HTML Goes Here.
+	$moduleTemplate .= "
+
+	function defaultPage() { return '{$pageName}'; } // defaultPage
 ";
-    }
-    else
-    {
-        $configFile = "";
 
-        $templateFile = "{* vim: set expandtab tabstop=4 shiftwidth=4 syntax=smarty: *}
-HTML Goes Here.
+	$moduleTemplate .= '
+} // module_' . $modName . '
+';
+
+	return $moduleTemplate;
+
+} // _moduleBit
+
+
+function _pageForModule($modName, $pageName) {
+
+	return '
+
+class module_' . $modName . '_' . $pageName . ' {
+
+	public function parameterList() {
+
+		return array();
+
+	} // parameterList
+
+
+	function parametersDidLoad($oPage, $aParams) {
+
+		//$oPage->sharedOutlet(\'paginator\')->readPaginatorStateFromParams($aParams);
+
+	} // parametersDidLoad
+
+
+	function noAction($oPage, $aParams) {
+
+		//$this->search($oPage, $aParams);
+
+	} // noAction
+
+
+	function setupSkin($oPage, $aParams, $oSkin) {
+
+		//$oSkin->addHeadString(\'<link rel="stylesheet" type="text/css" href="\' . $oSkin->getSkinDirShared() . \'/form.css" />\');
+
+		$oSkin->setTitle(\'' . $modName . ' : ' . $pageName . '\');
+
+	} // setupSkin
+
+} // module_' . $modName . '_' . $pageName . '
+';
+
+} // _pageForModule
+
+
+function createPage($pageName) {
+
+		$configFile = "---";
+
+		$templateFile = "{* vim: set expandtab tabstop=4 shiftwidth=4 syntax=smarty: *}
+Smarty HTML Goes Here.
 ";
-    }
 
-    if (!file_exists($pageName . '.tpl'))
-    {
-        print "Writing {$pageName}.tpl\n";
-        file_put_contents($pageName . '.tpl', $templateFile);
-    }
-    else
-    {
-        print "Skipping .tpl file because it already exists.\n";
-    }
+	if (!file_exists($pageName . '.tpl')) {
+		print "Writing {$pageName}.tpl\n";
+		file_put_contents($pageName . '.tpl', $templateFile);
+	} else {
+		print "Skipping .tpl file because it already exists.\n";
+	}
 
-    if (!file_exists($pageName . '.yaml'))
-    {
-        print "Writing {$pageName}.yaml\n";
-        file_put_contents($pageName . '.yaml', $configFile);
-    }
-    else
-    {
-        print "Skipping .yaml file because it already exists.\n";
-    }
+	if (!file_exists($pageName . '.yaml')) {
+		print "Writing {$pageName}.yaml\n";
+		file_put_contents($pageName . '.yaml', $configFile);
+	} else {
+		print "Skipping .yaml file because it already exists.\n";
+	}
 
-    print "Done!\n";
+	print "Done!\n";
 }
 ?>
